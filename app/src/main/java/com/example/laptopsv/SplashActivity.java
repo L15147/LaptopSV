40bd9047af8264f3398ea8478e938d1b0684cddf
LongDao29 <Longsama2909@gmail.com>
2021-11-02 14:21:07 +0700

    commit man hinh chao

---
+++ app/src/main/java/com/example/laptopsv/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.laptopsv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+
+}
